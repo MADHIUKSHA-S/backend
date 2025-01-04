cdfaeca898cb5a2f2f1f0692c8d9b5fb85aad7ea
MADHIUKSHA-S <madhiuksha551@gmail.com>
2025-01-04 22:46:28 +0530

    added some changes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -14,7 +14,7 @@ router.post('/register', async (req, res) => {
 
         const hashedPassword = await bcrypt.hash(password, 10);
         user = new User({ name, email, password: hashedPassword });
-
+        console.log(first);
         await user.save();
         res.status(201).json({ message: 'User registered successfully' });
     } catch (error) {
