18193989582688af4a0740f1e224bae683ed8c20
MADHIUKSHA-S <madhiuksha551@gmail.com>
2025-01-06 13:51:43 +0530

    cors

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ connectDB();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: 'https://frontend-xi-two-35.vercel.app/' }));
 
 // Routes
 app.use('/api/users', userRoutes);
